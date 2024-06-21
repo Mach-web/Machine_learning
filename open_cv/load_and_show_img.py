e454369ccdd613e3d7d982df6bc99f729f5317ff
Mach-web <mkanderson771@gmail.com>
2024-06-21 22:40:48 +0300

    Save image

--- open_cv/load_and_show_img.py
+++ open_cv/load_and_show_img.py
@@ -3,6 +3,12 @@ import cv2 as cv
 # flower = cv.imread("flower.jpg")
 # image = cv.imread("car.jpg")
 image = cv.imread("background.jpg")
-cv.imshow("window", image)
-cv.waitKey(10000)
+# cv.imshow("window", image)
+
+cv.imwrite("background.jpg", image)
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+cv.imwrite("background_grey.png", gray)
+cv.imshow("window", gray)
+
+cv.waitKey(30000)
 cv.destroyAllWindows()
