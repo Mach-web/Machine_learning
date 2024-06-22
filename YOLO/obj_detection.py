1c13fb1c8637d25f827c79a53f54e5660227d38a
Mach-web <mkanderson771@gmail.com>
2024-06-22 08:46:40 +0300

    Getting started with YOLO

--- YOLO/obj_detection.py
+++ YOLO/obj_detection.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")
-
+# use camera
 results = model(source=0,show = True, conf = 0.4, save = True)
+# use image in same folder
+# results = model(source="bus.jpg",show=True, conf = 0.4,save = False)
