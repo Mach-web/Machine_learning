d6d4e753d383f0f72d015ba29f0f1801f1f7bd85
Mach-web <mkanderson771@gmail.com>
2024-06-24 17:28:36 +0300

    Use Object Oriented Programming to run and stop the window

---
+++ open_cv/OOP.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+class App:
+    def __init__(self):
+        img = cv.imread("background1.jpg")
+        Window("window", img)
+
+    def run(self):
+        k = 0
+        # ord - returns ASCII
+        # this code exits when one presses "q"
+        while k != ord("q"):
+            k = cv.waitKey(0)
+            # chr - ASCII to value
+            print(k, chr(k))
+
+        cv.destroyAllWindows()
+
+class Window:
+    def __init__(self, winname, image):
+        self.winname = winname
+        self.image = image
+        cv.imshow(winname, image)
+
+if __name__ == "__main__":
+    App().run()
