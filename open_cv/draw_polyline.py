8e0519787bfe9318380ffbdcb04f3ed23ac8eb32
Mach-web <mkanderson771@gmail.com>
2024-06-28 20:35:59 +0300

    Fill the poly

--- open_cv/draw_polyline.py
+++ open_cv/draw_polyline.py
@@ -1,11 +1,14 @@
 import cv2 as cv
 import numpy as np
 
-img = np.zeros((400, 600, 3), np.uint8)
+img = np.zeros((500, 500, 3), np.uint8)
 pts = [(50, 50), (300, 190), (400, 10), (400, 400)]
 cv.polylines(img, np.array([pts]), True, (34, 67, 73), 5)
+cv.fillPoly(img, np.array([pts]), (34, 67, 73))
 
 cv.imshow("window", img)
 
 cv.waitKey(10000)
 cv.destroyAllWindows()
+
+
