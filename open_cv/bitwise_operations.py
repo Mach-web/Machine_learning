16b79d14c0be2840406f8823fdddd044f0fce6b3
Mach-web <mkanderson771@gmail.com>
2024-07-07 22:54:30 +0300

    Experimented with changing dimensions of the circle and rectangle

--- open_cv/bitwise_operations.py
+++ open_cv/bitwise_operations.py
@@ -1,3 +1,13 @@
 import cv2 as cv
 import numpy as np
 
+margin = 20
+rect = np.zeros((200, 200), np.uint8)
+cv.rectangle(rect, (margin, margin), (200-margin, 200-margin), 150, -1)
+
+circle = np.zeros((200, 200), np.uint8)
+cv.circle(circle, (100, 100), 80, 255, -1)
+
+cv.imshow("window", rect)
+cv.waitKey(10000)
+cv.destroyAllWindows()
