14b111a09c3ea5c10d25332380ee333f395e3c6c
Mach-web <mkanderson771@gmail.com>
2024-07-07 21:38:38 +0300

    Rotate images according to certain angle.

--- open_cv/rotation.py
+++ open_cv/rotation.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('car_gray.tiff')
+img = cv.imread('background1.jpg')
 
 print("Img shape: {} \nImg[:2]: {}".format(img.shape, img.shape[:2]))
 # get image height and width
@@ -9,5 +9,17 @@ height, width = img.shape[:2]
 center = (height // 2, width // 2)
 print(f"Center of Image: {center}")
 
-def trackbar(angle):
+'''M = cv.getRotationMatrix2D(center, -60, 0.7)
+rotated = cv.warpAffine(img, M, (width, height))
+cv.imshow("window", rotated)'''
 
+def track(angle):
+    M = cv.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv.warpAffine(img, M, (width, height))
+    cv.imshow("window", rotated)
+
+cv.imshow("window", img)
+cv.createTrackbar('angl', 'window', 0, 270, track)
+
+cv.waitKey(20000)
+cv.destroyAllWindows()
