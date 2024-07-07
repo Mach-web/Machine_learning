e14e3768e4a07589430470e28fe7d4f1fffd8a06
Mach-web <mkanderson771@gmail.com>
2024-07-07 23:06:35 +0300

    Perform bitwise operations on the shapes

--- open_cv/bitwise_operations.py
+++ open_cv/bitwise_operations.py
@@ -1,13 +1,19 @@
 import cv2 as cv
 import numpy as np
 
-margin = 20
+margin = 30
 rect = np.zeros((200, 200), np.uint8)
-cv.rectangle(rect, (margin, margin), (200-margin, 200-margin), 150, -1)
+cv.rectangle(rect, (margin, margin), (200-margin, 200-margin), 255, -1)
 
 circle = np.zeros((200, 200), np.uint8)
 cv.circle(circle, (100, 100), 80, 255, -1)
 
-cv.imshow("window", rect)
-cv.waitKey(10000)
+bit_and = cv.bitwise_and(rect, circle)
+bit_or = cv.bitwise_or(rect, circle)
+bit_xor = cv.bitwise_xor(rect, circle)
+
+img = np.hstack([rect, circle, bit_and, bit_or, bit_xor])
+
+cv.imshow("window", img)
+cv.waitKey(0000)
 cv.destroyAllWindows()
